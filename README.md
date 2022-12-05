10b8069329eed01ce8297e802148101411e4dc1a
Mike <xxmixanikxx@gmail.com>
2022-12-05 03:35:12 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # chat-gpt
 Simple javascript wrapper for ChatGPT's unofficial web API
+
+## Installation
+`npm i chat-gpt`
+
+## Setup & Usage
+1. You will need session token for this to work.  
+To obtain it:
+   - go to https://chat.openai.com/chat and login
+   - press F12 to open browser devtools and go to `Application` tab
+   - find `Cookies` in `Storage` section, open them up and click on `https://chat.openai.com`
+   - find cookie with name `__Secure-next-auth.session-token` and copy its value
+2. Create file `config.json` and paste your session token as `SessionToken` key value:
+```json
+{
+  "SessionToken": "<insert-your-session-token-here>"
+}
+```
+3. Here's minimal code for prompting a question to chatGPT
+```js
+const cgpt = require('chat-gpt');
+const config = require('./config');
+
+const chatbot = new cgpt.ChatGPT(config);
+chatbot.ask("Hey, how are you doing today?").then(answer => {
+    console.log(answer);
+})
+```
+
+## Credits
+Inspired by python version - https://github.com/acheong08/ChatGPT  
+ChatGPT for creating model - https://chat.openai.com/chat  
+
+funny fact, initial code was written by giving and asking ChatGPT to rewrite python version in javascript
---
+++ index.js
@@ -0,0 +1,89 @@
+const uuid = require('uuid');
+const axios = require('axios');
+
+
+class ChatGPT {
+    constructor(config, conversationId = null) {
+        this.config = config;
+        this.conversationId = conversationId;
+        this.parentId = uuid.v4();
+    }
+
+    async ask(prompt) {
+        if (!this.config.Authorization || !this.validateToken(this.config.Authorization))
+            await this.getTokens();
+
+        let response = await axios.request({
+            method: 'POST',
+            url: 'https://chat.openai.com/backend-api/conversation',
+            data: {
+                action: 'next',
+                messages: [
+                    {
+                        id: uuid.v4(),
+                        role: 'user',
+                        content: {
+                            content_type: 'text',
+                            parts: [prompt],
+                        },
+                    },
+                ],
+                model: 'text-davinci-002-render',
+                conversation_id: this.conversationId,
+                parent_message_id: this.parentId,
+            },
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${this.config.Authorization}`,
+                'Content-Type': 'application/json',
+            }
+        });
+
+        try {
+            const parts = response.data.split('\n');
+            response = JSON.parse(parts[parts.length-5].split('data: ')[1]);
+        } catch (err) {
+            throw new Error(`Could not find or parse actual response text due to: ${err}`);
+        }
+
+        this.parentId = response.message.id;
+        this.conversationId = response.conversation_id;
+
+        return response.message.content.parts[0];
+    }
+
+    validateToken(token) {
+        if (!token) return false;
+        const parsed = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+
+        return Date.now() <= parsed.exp * 1000;
+    }
+
+    async getTokens() {
+        if (!this.config.hasOwnProperty('SessionToken')) {
+            throw new Error('No session token provided');
+        }
+
+        const response = await axios.request({
+            method: 'GET',
+            url: 'https://chat.openai.com/api/auth/session',
+            headers: {
+                'Cookie': `__Secure-next-auth.session-token=${this.config.SessionToken}`
+            }
+        });
+
+        try {
+            const cookies = response.headers['set-cookie'];
+            const sessionCookie = cookies.find(cookie => cookie.startsWith('__Secure-next-auth.session-token'));
+
+            this.config.SessionToken = sessionCookie.split('=')[1];
+            this.config.Authorization = response.data.accessToken;
+        } catch (err) {
+            throw new Error(`Failed to fetch new session tokens due to: ${err}`);
+        }
+    }
+}
+
+module.exports = {
+    ChatGPT
+};
